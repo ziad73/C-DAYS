fa471a3cfa665dfb5b20c0ba50014e1b133f3870
ziadelsayd <ziadelsayedx@gmail.com>
2025-07-21 00:52:21 +0300

    Make the relationship between Employee and Project as M to M

---
+++ C#/DAY5/Company System Project/Company System/EmployeeProjects/EmployeeProjectAssignment.cs
@@ -0,0 +1,11 @@
+﻿namespace Company_System.EmployeeProjects
+{
+    public class EmployeeProjectAssignment
+    {
+        public int EmployeeId { get; set; }
+        public int ProjectId { get; set; }
+        public double Hours { get; set; }
+        public DateTime AssignmentDate { get; set; } = DateTime.Now;
+
+    }
+}
